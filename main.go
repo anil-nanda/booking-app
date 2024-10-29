9ac589a765686dec7a41e1237bafad73eef0ad96
Anil Nanda <anilthekkoote@gmail.com>
2024-10-29 19:25:52 +0400

    variables and constants
    variables, constants and printf

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welocome to the", conferenceName)
+	fmt.Println("We have",remainingTickets, "remaining")
+
+	//using printf instead of Println
+	fmt.Printf("Get your tickets for %s here\n", conferenceName)
+
+	
 }
