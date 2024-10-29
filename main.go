fecce0c54c3687ea76d086c7a9fd2de1e080864b
Anil Nanda <anilthekkoote@gmail.com>
2024-10-29 21:07:30 +0400

    array and slices

--- main.go
+++ main.go
@@ -28,5 +28,29 @@ func main() {
 	fmt.Println("Enter the number of tickets: ")
 	fmt.Scan(&userTickets)
 	fmt.Printf("user %v booked %v tickets\n", userName,userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	// define and array of size 50 with type string
+	var bookings = [50]string{}
+
+	// define array with elements
+	var arr = [50]string{"a","b","c"}
+
+	fmt.Printf("%v\n",arr)
+
+	// add element to array
+	bookings[0] = userName
+
+	fmt.Printf("Whole array %v\n",bookings)
+	fmt.Printf("Length of array is %v\n", len(bookings))
+	fmt.Printf("Type of array is %T\n", bookings)
+	fmt.Printf("First element of array is %v\n", bookings[0])
+
+
+	//Slice is an array without size definition
+	var booked = []string{}
+	booked  = append(booked, userName)
+	fmt.Printf("print slice %v\n", booked)
 	
 }
