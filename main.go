95134768f92610740434cf5e5c8d8f461d68bccd
Anil Nanda <anilthekkoote@gmail.com>
2024-10-30 08:06:31 +0400

    more if else statements

--- main.go
+++ main.go
@@ -29,41 +29,63 @@ func main() {
 		fmt.Scan(&lastName)
 		fmt.Println("Enter the email: ")
 		fmt.Scan(&email)
+		fmt.Println("Enter the city: ")
+		fmt.Scan(&city)
 		fmt.Println("Enter the number of tickets: ")
 		fmt.Scan(&userTickets)
+
+		isValidName := len(firstName) >=2 && len(lastName) >=2
+
+		isValidEmail := strings.Containts(email, "@")
+
+		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
+
+		isValidCity := city == 'Singapore' || city == 'London'
 		
-		if userTickets > remainingTickets {
-			fmt.Printf("we only have %v tickets remaining", remainingTickets)
-			continue
+		if isValidName && isValidEmail && isValidTickets && isValidCity {
+			fmt.Printf("user %v %v booked %v tickets. confirmation mail send to %v\n", firstName, lastName,userTickets, email)
+		
+			remainingTickets = remainingTickets - userTickets
+
+			fmt.Printf("%v tickets remaining\n", remainingTickets)
+
+			bookings  = append(bookings, firstName + " "+ lastName)
+		
+			fmt.Printf("First element of slice is %v\n", bookings[0])
+			//fmt.Printf("Type of slice is %T\n", bookings)
+
+			firstNames := []string{}
+			// _ is used to mention an unused variable as index is not reffered in the loop 
+			for _, booking := range bookings {
+				names := strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("First names of the booking are %v\n", firstNames)
+			fmt.Printf("Whole booking list %v\n",bookings)
+			fmt.Printf("Number of unique bookings is %v\n", len(bookings))
+
+			noTicketsRemaining := remainingTickets == 0
+
+			if noTicketsRemaining {
+				fmt.Printf("All tickets are sold out")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Printf("Entered name is not valid")
+			}
+			if !isValidEmail {
+				fmt.Printf("Email is not valid")
+			}
+			if !isValidCity {
+				fmt.Printf("City is not valid")
+			}
+			if !isValidTickets {
+				fmt.Printf("Tickets is not valid")
+			}
 		}
 
-		fmt.Printf("user %v %v booked %v tickets. confirmation mail send to %v\n", firstName, lastName,userTickets, email)
 		
-		remainingTickets = remainingTickets - userTickets
-
-		fmt.Printf("%v tickets remaining\n", remainingTickets)
-
-		bookings  = append(bookings, firstName + " "+ lastName)
-		
-		fmt.Printf("First element of slice is %v\n", bookings[0])
-		//fmt.Printf("Type of slice is %T\n", bookings)
-
-		firstNames := []string{}
-		// _ is used to mention an unused variable as index is not reffered in the loop 
-		for _, booking := range bookings {
-			names := strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("First names of the booking are %v\n", firstNames)
-		fmt.Printf("Whole booking list %v\n",bookings)
-		fmt.Printf("Number of unique bookings is %v\n", len(bookings))
-
-		noTicketsRemaining := remainingTickets == 0
-
-		if noTicketsRemaining {
-			fmt.Printf("All tickets are sold out")
-			break
-		}
 	}
 	
 }
